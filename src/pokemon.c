5956990b3e960ff70f88dc13b330a183defaeb6f
MeekRhino <challenert@gmail.com>
2021-09-08 02:24:54 -0500

    Implement Iron Will ability.

--- src/pokemon.c
+++ src/pokemon.c
@@ -3158,6 +3158,8 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
         gBattleMovePower = (150 * gBattleMovePower) / 100;
     if (type == TYPE_DRAGON && attacker->ability == ABILITY_DRAGON_BLOOD && attacker->hp <= (attacker->maxHP / 3))
         gBattleMovePower = (150 * gBattleMovePower) / 100;
+    if (type == TYPE_STEEL && attacker->ability == ABILITY_IRON_WILL && attacker->hp <= (attacker->maxHP / 3))
+        gBattleMovePower = (150 * gBattleMovePower) / 100;
     if (type == TYPE_FIRE && attacker->ability == ABILITY_BLAZE && attacker->hp <= (attacker->maxHP / 3))
         gBattleMovePower = (150 * gBattleMovePower) / 100;
     if (type == TYPE_WATER && attacker->ability == ABILITY_TORRENT && attacker->hp <= (attacker->maxHP / 3))
