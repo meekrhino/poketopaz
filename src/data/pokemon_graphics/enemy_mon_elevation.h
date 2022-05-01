66eb327c766d5c32c699985f9ff70a6b1ec8166e
MeekRhino <challenert@gmail.com>
2022-05-01 00:08:55 -0500

    Add enemy elevation values for new mons.

--- src/data/pokemon_graphics/enemy_mon_elevation.h
+++ src/data/pokemon_graphics/enemy_mon_elevation.h
@@ -2,12 +2,27 @@
 // is during battle. Species that float or fly have nonzero values.
 const u8 gEnemyMonElevation[NUM_SPECIES] =
 {
+    [SPECIES_EAGLAIR] = 7,
+    [SPECIES_GRIFIC] = 4,
+    [SPECIES_PHANTERN] = 4,
+    [SPECIES_SPIDREAM] = 8,
+    [SPECIES_DUSKULL] = 9,
     [SPECIES_VENOMOTH] = 8,
     [SPECIES_KOFFING] = 8,
     [SPECIES_WEEZING] = 6,
     [SPECIES_YANMA] = 8,
-    [SPECIES_DUSKULL] = 9,
+    [SPECIES_SERAPH] = 6,
+    [SPECIES_CIRROFT] = 12,
+    [SPECIES_CUMULOFT] = 8,
+    [SPECIES_FROSTORM] = 6,
+    [SPECIES_NIMBOLT] = 6,
     [SPECIES_MISDREAVUS] = 8,
+    [SPECIES_BENENGIL] = 6,
+    [SPECIES_MIRUST] = 8,
+    [SPECIES_FERRIAN] = 6,
     [SPECIES_FLYGON] = 7,
+    [SPECIES_MISTICADE] = 6,
+    [SPECIES_KURANPU] = 10,
+    [SPECIES_KIPPOROK] = 11,
     [SPECIES_JIRACHI] = 12,
 };
