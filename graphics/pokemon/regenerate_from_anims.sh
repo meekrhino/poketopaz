208cf43c7919b3ec9449b46e06a163a3dd58ce98
meekrhino <challenert@gmail.com>
2023-11-08 14:47:42 -0600

    Support generating frames and gifs from anim_front

--- graphics/pokemon/regenerate_from_anims.sh
+++ graphics/pokemon/regenerate_from_anims.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-if [ -n "$1" ]; then
-    p="$1"
+process_p() {
+    local p="$1"
     echo "Processing with parameter: $p"
     
     sudo chmod +777 "$p/front.png"
     sudo chmod +777 "$p/front.4bpp"
+    sudo chmod +777 "$p/front-frame-2.png"
+    sudo chmod +777 "$p/front-frame-2.4bpp"
     sudo chmod +777 "$p/front-shiny.png"
+    sudo chmod +777 "$p/front-shiny-frame-2.png"
     sudo chmod +777 "$p/anim_front.4bpp"
     sudo chmod +777 "$p/anim_front.png"
     sudo chmod +777 "$p/anim_front_shiny.png"
@@ -14,7 +17,10 @@ if [ -n "$1" ]; then
     
     sudo rm "$p/front.png"
     sudo rm "$p/front.4bpp"
+    sudo rm "$p/front-frame-2.png"
+    sudo rm "$p/front-frame-2.4bpp"
     sudo rm "$p/front-shiny.png"
+    sudo rm "$p/front-shiny-frame-2.png"
     sudo rm "$p/anim_front.4bpp"
     sudo rm "$p/anim_front_shiny.png"
     sudo rm "$p/anim_front_shiny.4bpp"
@@ -25,40 +31,24 @@ if [ -n "$1" ]; then
     
     file_size=$(($(stat -c %s "$p/anim_front.4bpp") / 2))
     sudo dd if="$p/anim_front.4bpp" bs="$file_size" count=1 > "$p/front.4bpp"
+    sudo dd if="$p/anim_front.4bpp" bs="$file_size" skip=1 > "$p/front-frame-2.4bpp"
     
     sudo ../../tools/gbagfx/gbagfx "$p/anim_front.4bpp" "$p/anim_front_shiny.png" -palette "$p/shiny.pal" -object -width 8
     sudo ../../tools/gbagfx/gbagfx "$p/front.4bpp" "$p/front.png" -palette "$p/normal.pal" -object -width 8
+    sudo ../../tools/gbagfx/gbagfx "$p/front-frame-2.4bpp" "$p/front-frame-2.png" -palette "$p/normal.pal" -object -width 8
     sudo ../../tools/gbagfx/gbagfx "$p/front.4bpp" "$p/front-shiny.png" -palette "$p/shiny.pal" -object -width 8
+    sudo ../../tools/gbagfx/gbagfx "$p/front-frame-2.4bpp" "$p/front-shiny-frame-2.png" -palette "$p/shiny.pal" -object -width 8
     sudo ../../tools/gbagfx/gbagfx "$p/back.4bpp" "$p/back-shiny.png" -palette "$p/shiny.pal" -object -width 8
+    
+    convert -dispose Background -delay 100 -loop 0 "$p/front.png" "$p/front-frame-2.png" "$p/anim_front.gif"
+    convert -dispose Background -delay 100 -loop 0 "$p/front-shiny.png" "$p/front-shiny-frame-2.png" "$p/anim_front_shiny.gif"
+}
+
+if [ -n "$1" ]; then
+    p="$1"
+    process_p "$p"
 else
     while read -r p; do
-        echo "Processing: $p"
-        
-        sudo chmod +777 "$p/front.png"
-        sudo chmod +777 "$p/front.4bpp"
-        sudo chmod +777 "$p/front-shiny.png"
-        sudo chmod +777 "$p/anim_front.4bpp"
-        sudo chmod +777 "$p/anim_front.png"
-        sudo chmod +777 "$p/anim_front_shiny.png"
-        sudo chmod +777 "$p/back.4bpp"
-        
-        sudo rm "$p/front.png"
-        sudo rm "$p/front.4bpp"
-        sudo rm "$p/front-shiny.png"
-        sudo rm "$p/anim_front.4bpp"
-        sudo rm "$p/anim_front_shiny.png"
-        sudo rm "$p/anim_front_shiny.4bpp"
-        sudo rm "$p/back.4bpp"
-        
-        sudo ../../tools/gbagfx/gbagfx "$p/anim_front.png" "$p/anim_front.4bpp"
-        sudo ../../tools/gbagfx/gbagfx "$p/back.png" "$p/back.4bpp"
-        
-        file_size=$(($(stat -c %s "$p/anim_front.4bpp") / 2))
-        sudo dd if="$p/anim_front.4bpp" bs="$file_size" count=1 > "$p/front.4bpp"
-        
-        sudo ../../tools/gbagfx/gbagfx "$p/anim_front.4bpp" "$p/anim_front_shiny.png" -palette "$p/shiny.pal" -object -width 8
-        sudo ../../tools/gbagfx/gbagfx "$p/front.4bpp" "$p/front.png" -palette "$p/normal.pal" -object -width 8
-        sudo ../../tools/gbagfx/gbagfx "$p/front.4bpp" "$p/front-shiny.png" -palette "$p/shiny.pal" -object -width 8
-        sudo ../../tools/gbagfx/gbagfx "$p/back.4bpp" "$p/back-shiny.png" -palette "$p/shiny.pal" -object -width 8
+        process_p "$p"
     done < pokemonlist-new.txt
 fi
