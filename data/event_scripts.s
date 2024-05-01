db2d76519a056814c02f947050ac22407ef452ba
meekrhino <challenert@gmail.com>
2024-05-01 15:59:25 -0500

    Mass capitalization adjustment.  Small map/quest cleanup.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -858,7 +858,7 @@ gText_SandstormIsVicious::
 	.string "It's impossible to keep going.$"
 
 gText_SelectWithoutRegisteredItem::
-	.string "An item in the BAG can be\n"
+	.string "An item in the Bag can be\n"
 	.string "registered to SELECT for easy use.$"
 
 gText_PokemonTrainerSchoolEmail::
